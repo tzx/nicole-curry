5bc23b53b4589ddd125c2d4f0902d3415e6b4a9f
Timmy Xiao <34635512+tzx@users.noreply.github.com>
2022-12-10 22:35:30 -0500

    Update search to only allow results with multiword

--- src/search.ts
+++ src/search.ts
@@ -1,6 +1,43 @@
 import defaultWordTrie from "./dictionary";
 import { DIR } from "./zus/BoardStore";
 
+interface SearchResult {
+  row: number,
+  col: number,
+  direction: DIR,
+  word: string,
+}
+
+const validCross = (
+  board: string[][],
+  row: number,
+  col: number,
+  direction: DIR,
+  char: string,
+  wordTrie = defaultWordTrie,
+) => {
+  // Use the opposite direction
+  const dirChange = direction === DIR.VERTICAL ? [0, 1] : [1, 0];
+  const prev = [];
+  while (row - dirChange[0] >= 0 && col - dirChange[1] >= 0 && board[row - dirChange[0]][col - dirChange[1]] !== "") {
+    row = row - dirChange[0];
+    col = col - dirChange[1];
+    prev.push(board[row][col]);
+  }
+  const forward = [];
+  while (row + dirChange[0] < board.length && col + dirChange[1] < board[0].length && board[row + dirChange[0]][col + dirChange[1]] !== "") {
+    row = row + dirChange[0];
+    col = col + dirChange[1];
+    forward.push(board[row][col]);
+  }
+  const word = [...prev.reverse(), char, ...forward].join("");
+  if (word.length == 1) {
+    return true;
+  }
+
+  return wordTrie.has(word);
+}
+
 const newRowCol = (row: number, col: number, direction: DIR) => {
   if (direction == DIR.VERTICAL) {
     return [row + 1, col];
@@ -18,6 +55,7 @@ export const search = (
   results: string[],
   wordTrie = defaultWordTrie,
 ) => {
+  // TODO: needs to touch
   const buildString = build.join("");
   if (row >= board.length || col >= board[0].length) {
     if (wordTrie.has(buildString)) {
@@ -49,6 +87,9 @@ export const search = (
   for (let i = 0; i < letters.length; i++) {
     const char = letters[i];
     const rest = [...letters.slice(0, i), ...letters.slice(i + 1)];
+    if (!validCross(board, row, col, direction, char, wordTrie)) {
+      continue;
+    }
     board[row][col] = char;
     build.push(char);
     search(
@@ -65,3 +106,42 @@ export const search = (
     board[row][col] = "";
   }
 }
+
+export const searchBoard = (
+  board: string[][],
+  letters: string[],
+  wordTrie = defaultWordTrie,
+) => {
+  const words: SearchResult[] = [];
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board[0].length; c++) {
+      if (r === 0 || board[r-1][c] == "") {
+        const vertResult: string[] = [];
+        search(board, letters, DIR.VERTICAL, [], r, c, vertResult, wordTrie);
+        const vr = vertResult.map((word) => {
+          return {
+            row: r,
+            col: c,
+            direction: DIR.VERTICAL,
+            word,
+          }
+        });
+        words.push(...vr);
+      }
+      if (c === 0 || board[r][c-1] == "") {
+        const horResult: string[] = [];
+        search(board, letters, DIR.HORIZONTAL, [], r, c, horResult, wordTrie);
+        const hr = horResult.map((word) => {
+          return {
+            row: r,
+            col: c,
+            direction: DIR.HORIZONTAL,
+            word,
+          }
+        });
+        words.push(...hr);
+      }
+    }
+  }
+  return words;
+};
--- test/search.test.ts
+++ test/search.test.ts
@@ -1,5 +1,5 @@
 import { assert, expect, test } from "vitest";
-import { search } from "../src/search";
+import { search, searchBoard } from "../src/search";
 import Trie from "mnemonist/trie";
 import { DIR } from "../src/zus/BoardStore";
 
@@ -11,6 +11,7 @@ test('Simple Row Search', () => {
   const results = [];
   search(board, ["p", "a", "t", "d"], DIR.HORIZONTAL, [], 0, 0, results, trie);
 
+  console.log(results);
   assert(results.length === 1);
   expect(results[0]).eq("cat");
 })
@@ -27,7 +28,7 @@ test('Simple Column Search', () => {
   expect(results[0]).eq("doge");
 })
 
-test ('Two row words: one goes to bounds, one before bounds', () => {
+test('Two row words: one goes to bounds, one before bounds', () => {
   const trie = Trie.from(["cat", "cape", "dog", "cone"]);
   const board = Array.from({length: 3}, () => Array.from({length:4}, () => ""));
   board[0][0] = "c";
@@ -36,4 +37,43 @@ test ('Two row words: one goes to bounds, one before bounds', () => {
   search(board, ["p", "a", "t", "d", "e"], DIR.HORIZONTAL, [], 0, 0, results, trie);
 
   expect(results).to.have.members(["cat", "cape"]);
+})
+
+test('Not searching empty space before horizontal', () => {
+  const trie = Trie.from(["cat", "at"]);
+  const board = Array.from({length: 1}, () => Array.from({length:4}, () => ""));
+  board[0][1] = "c";
+  const words = searchBoard(board, ["c", "a", "t"], trie);
+  // At shouldn't be there
+  const expected = [{ row: 0, col: 1, direction: DIR.HORIZONTAL, word: "cat" }];
+  assert.sameDeepMembers(words, expected);
+})
+
+test('Cross Search', () => {
+  const trie = Trie.from(["cat", "ct", "at"]);
+  const board = Array.from({length: 3}, () => Array.from({length:3}, () => ""));
+  board[1][0] = "t";
+  board[1][1] = "t";
+  const words = searchBoard(board, ["c", "a", "t"], trie);
+  console.log(words);
+  const expected = [
+                    { row: 0, col: 0, direction: DIR.HORIZONTAL, word: "cat" },
+                    { row: 0, col: 0, direction: DIR.VERTICAL, word: "ct" },
+                    { row: 0, col: 1, direction: DIR.VERTICAL, word: "ct" },
+                    { row: 0, col: 1, direction: DIR.VERTICAL, word: "at" },
+                    { row: 0, col: 0, direction: DIR.VERTICAL, word: "at" },
+                    { row: 0, col: 1, direction: DIR.HORIZONTAL, word: "at" },
+                    { row: 0, col: 1, direction: DIR.HORIZONTAL, word: "ct" },
+                   ];
+  assert.sameDeepMembers(words, expected);
+})
+
+test('Cross Search prevents', () => {
+  const trie = Trie.from(["tea"]);
+  const board = Array.from({length: 3}, () => Array.from({length:3}, () => ""));
+  board[1][0] = "t";
+  board[1][1] = "t";
+  board[1][2] = "t";
+  const words = searchBoard(board, ["t", "e", "a"], trie);
+  assert(words.length === 0);
 })
